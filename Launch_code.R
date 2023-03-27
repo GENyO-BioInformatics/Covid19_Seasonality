4fee917bf21352e7ed6db7c482cec1ed652fd2c1
Juananvg <juanantoniovillatorogarcia@gmail.com>
2023-03-27 10:11:42 +0200

    Update code

--- Launch_code.R
+++ Launch_code.R
@@ -11,24 +11,16 @@ dir.create("Tables")
 
 
 #Spain code
-system("Rscript RawData/Process_Spain_Raw.R")
 system("Rscript Code/Spain_models.R")
 system("Rscript Code/Spain_Figures.R")
 
+
+#Europe code
+system("Rscript Code/Europe_models.R")
+system("Rscript Code/Europe_Figures.R")
+
 #Italy code
-system("Rscript RawData/Process_Italy_Raw.R")
 system("Rscript Code/Italy_models.R")
 system("Rscript Code/Italy_Figures.R")
 
 
-#Europe code
-system("Rscript RawData/Process_Europe_Raw.R")
-system("Rscript Code/Europe_models.R")
-system("Rscript Code/Europe_Figures.R")
-
-
-
-#USA code
-system("Rscript RawData/Process_USA_Raw.R")
-system("Rscript Code/USA_models.R")
-system("Rscript Code/USA_Figures.R")
